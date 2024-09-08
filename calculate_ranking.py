b96927e77d892c6b22bee59e2837077eb353dbd3
thancock-silentfalcon <thancock@silentfalconuas.com>
2024-09-08 14:12:16 -0600

    add team data

---
+++ calculate_ranking.py
@@ -0,0 +1,19 @@
+#Team Data
+team_data = [
+    {'team': 'Arizona', 'abbreviation': 'UA', 'logo_path': 'big 12 logos/ua.png'},
+    {'team': 'Arizona State', 'abbreviation': 'ASU', 'logo_path': 'big 12 logos/asu.png'},
+    {'team': 'Baylor', 'abbreviation': 'BU', 'logo_path': 'big 12 logos/bu.png'},
+    {'team': 'BYU', 'abbreviation': 'BYU', 'logo_path': 'big 12 logos/byu.png'},
+    {'team': 'Cincinnati', 'abbreviation': 'UC', 'logo_path': 'big 12 logos/uc.png'},
+    {'team': 'Colorado', 'abbreviation': 'CU', 'logo_path': 'big 12 logos/cu.png'},
+    {'team': 'Houston', 'abbreviation': 'UH', 'logo_path': 'big 12 logos/uh.png'},
+    {'team': 'Iowa State', 'abbreviation': 'ISU', 'logo_path': 'big 12 logos/isu.png'},
+    {'team': 'Kansas', 'abbreviation': 'KU', 'logo_path': 'big 12 logos/ku.png'},
+    {'team': 'Kansas State', 'abbreviation': 'KSU', 'logo_path': 'big 12 logos/ksu.png'},
+    {'team': 'Oklahoma State', 'abbreviation': 'OSU', 'logo_path': 'big 12 logos/osu.png'},
+    {'team': 'TCU', 'abbreviation': 'TCU', 'logo_path': 'big 12 logos/tcu.png'},
+    {'team': 'Texas Tech', 'abbreviation': 'TTU', 'logo_path': 'big 12 logos/ttu.png'},
+    {'team': 'UCF', 'abbreviation': 'UCF', 'logo_path': 'big 12 logos/ucf.png'},
+    {'team': 'Utah', 'abbreviation': 'UU', 'logo_path': 'big 12 logos/uu.png'},
+    {'team': 'West Virginia', 'abbreviation': 'WVU', 'logo_path': 'big 12 logos/wvu.png'}
+]
